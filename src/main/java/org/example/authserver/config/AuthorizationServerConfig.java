dc97787b8842cdbd0897d2607a6afa88347ba1f0
ngocson741852963 <117715756+ngocson741852963@users.noreply.github.com>
2024-10-13 22:11:02 +0700

    feature: cors

--- src/main/java/org/example/authserver/config/AuthorizationServerConfig.java
+++ src/main/java/org/example/authserver/config/AuthorizationServerConfig.java
@@ -154,6 +154,7 @@ public class AuthorizationServerConfig {
                 .anyRequest().authenticated()
         );
         http.addFilterBefore(myCorsFilter, ChannelProcessingFilter.class);
+
         return http.build();
     }
 
